21b1aa8bc49bfd9ab8ad566826e88406f9b96cff
Dononono <54520381+Dononono@users.noreply.github.com>
2019-09-20 23:01:52 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,77 @@
+input.onButtonPressed(Button.A, () => {
+    basic.showString("You get an A!")
+})
+input.onButtonPressed(Button.B, () => {
+    basic.showLeds(`
+         # # # # # 
+         # . . . # 
+         # . . . # 
+         # . . . # 
+         # # # # # 
+        `)
+    basic.showLeds(`
+        # # # . .
+        # . . # .
+        # # # . .
+        # . . # .
+        # # # . .
+        `)
+
+
+
+})
+input.onButtonPressed(Button.AB, () => {
+    basic.showLeds(`
+        . . . . . .  
+        . . . . . .
+        # . . . . # 
+        . . . . . . 
+        . . . . . . 
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+
+
+})
+input.onGesture(Gesture.Shake, () => {
+    basic.showLeds(`
+    . . . . . 
+    # . . . # 
+    . . . . . 
+    . # . # . 
+    # . # . # 
+
+    `)
+    basic.showLeds(`
+    . . . . .
+    # # . # #
+    . . . . .
+    # . # . # 
+    . # . # .
+    `)
+    basic.showLeds(`
+    . . . . .
+    # . . . #
+    . . . . .
+    # # # # #
+    . . . . .
+    `)
+
+    basic.clearScreen()
+
+
+
+})
+
